5206f994e0b4c3caab9fa5fea600d0c98cf9443a
Thaislaine Reis <thaiserica1304@gmail.com>
2025-12-12 13:28:16 -0300

    feat: add card search by name

--- src/javascript/script.js
+++ src/javascript/script.js
@@ -1,37 +1,32 @@
+let listaDeCartas = [];
+const secao = document.querySelector(".content-cards");
+const campoDeEntrada = document.getElementById('input-search');
+
 async function carregarCartas() {
     try {
         const resposta = await fetch('data/card.json');
         const cartas = await resposta.json();
 
-
-        console.log(cartas);
-
+        listaDeCartas = cartas;
 
         exibirCartas(cartas);
-
 
     } catch (erro) {
         console.error("Erro ao carregar cartas:", erro);
     }
 }
 
-
 function exibirCartas(lista) {
-    const secao = document.querySelector(".content-cards")
-
 
     lista.forEach(carta => {
         const div = document.createElement("div");
         div.classList.add("card");
 
-
         const img = document.createElement("img");
         img.src = `https://lorassets.switchblade.xyz/pt_br/img/cards/${carta.cardCode}.png`;
 
-
         const nome = document.createElement("h3");
         nome.textContent = carta.name;
-
 
         div.appendChild(img);
         div.appendChild(nome);
@@ -39,5 +34,31 @@ function exibirCartas(lista) {
     });
 }
 
+function pesquisarCarta() {
+    const cartaDigitada = campoDeEntrada.value.trim();
+
+    if (cartaDigitada === "") {
+        secao.innerHTML = "";
+        exibirCartas(listaDeCartas);
+        return;
+    };
+
+    const listaFiltrada = listaDeCartas.filter(carta =>
+        carta.name.toLowerCase().includes(cartaDigitada.toLowerCase())
+    );
+
+
+    secao.innerHTML = "";
+
+    if (listaFiltrada.length === 0) {
+        const p = document.createElement('p');
+        p.textContent = "Carta não encontrada.";
+        secao.appendChild(p);
+    } else {
+        exibirCartas(listaFiltrada);
+    }
+}
+
+campoDeEntrada.addEventListener('input', pesquisarCarta);
 
 carregarCartas();
